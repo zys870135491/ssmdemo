45f1320bcf06c941135566d70262d816b0708eb6
zhangyisong <zhangyisong@erry.com>
2019-09-03 15:13:21 +0800

    DeptMapper文件

--- src/main/java/com/ssmdemo/entity/Dept.java
+++ src/main/java/com/ssmdemo/entity/Dept.java
@@ -1,4 +1,34 @@
 package com.ssmdemo.entity;
 
 public class Dept {
+
+    private Integer id;
+    private String deptName;
+
+    public Dept() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDeptName() {
+        return deptName;
+    }
+
+    public void setDeptName(String deptName) {
+        this.deptName = deptName;
+    }
+
+    @Override
+    public String toString() {
+        return "Dept{" +
+                "id=" + id +
+                ", deptName='" + deptName + '\'' +
+                '}';
+    }
 }
